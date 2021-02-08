c791c4ec2f5278d91504c663c1f6555121c48cb6
kandh <kandh@danal.co.kr>
2021-02-08 20:57:06 +0900

    todo list 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [userName, SetUserName] = useState("Adam");
+  const [newItemText, SetNewItemText] = useState("");
+  const [todoItems, setTodoItems] = useState([
+    { action: "buy flowers", done: false },
+    { action: "get shoes", done: false },
+    { action: "collect ticket", done: true },
+    { action: "call joe", done: false },
+  ]);
+
+  const changeStateData = () => {
+    SetUserName(userName === "Adam" ? "Bob" : "Adam");
+  };
+
+  const updateNewTextValue = (event) => {
+    SetNewItemText(event.target.value);
+  };
+
+  const createNewTodo = () => {
+    setTodoItems([...todoItems, { action: newItemText, done: false }]);
+  };
+
+  const toggleTodo = (todo) => {
+    todoItems.map((item) =>
+      item.action === todo.action ? { ...item, done: !item.done } : item
+    );
+  };
+
+  const todoTableRows = () => {
+    return todoItems.map((item, index) => (
+      <tr key={index}>
+        <td>{item.action}</td>
+        <td>
+          <input
+            type="checkbox"
+            checked={item.done}
+            onChange={(item) => toggleTodo(item)}
+          />
+        </td>
+      </tr>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h4 className="bg-primary text-white text-center p-2">
+        {userName} To Do List ({todoItems.filter((t) => !t.done).length} items
+        to do)
+      </h4>
+      <div className="container-fluid">
+        <div className="my-1">
+          <input
+            className="form-control"
+            value={newItemText}
+            onChange={updateNewTextValue}
+          />
+          <button className="btn btn-primary mt-1" onClick={createNewTodo}>
+            Add
+          </button>
+        </div>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>{todoTableRows()}</tbody>
+        </table>
+      </div>
+      <button className="btn btn-primary m-2" onClick={changeStateData}>
+        Change
+      </button>
     </div>
   );
 }
